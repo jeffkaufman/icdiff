5af6255156938663f6020c4dc1959c42d77f7e1c
Jeff Kaufman <jeff@jefftk.com>
2014-12-08 19:09:41 -0500

    Add setuptools infrastructure.  Squash-merge of macro1's work in #9.

---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.util import convert_path
+import setuptools
+
+package_data = {}
+execfile('icdiff', package_data)
+
+setuptools.setup(
+    name="icdiff",
+    version=package_data.get('__version__'),
+    url="http://www.jefftk.com/icdiff",
+    author="Jeff Kaufman",
+    author_email="jeff@jefftk.com",
+    description="improved colored diff",
+    long_description=open('README').read(),
+    scripts=['icdiff', 'git-icdiff'],
+)
