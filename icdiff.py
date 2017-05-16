1a04ff032911521a8ea7329d9b32c3a4aa5ac731
Jasminderpal Singh Sidhu <jammy.sidhu43@gmail.com>
2017-05-16 20:30:53 +0530

    change except block exception handling

--- icdiff.py
+++ icdiff.py
@@ -430,13 +430,13 @@ class PassThroughOptionParser(OptionParser):
     def _process_long_opt(self, rargs, values):
         try:
             OptionParser._process_long_opt(self, rargs, values)
-        except BadOptionError, err:
+        except BadOptionError as err:
             self.largs.append(err.opt_str)
 
     def _process_short_opts(self, rargs, values):
         try:
             OptionParser._process_short_opts(self, rargs, values)
-        except BadOptionError, err:
+        except BadOptionError as err:
             self.largs.append(err.opt_str)
 
 def get_options():
