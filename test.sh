32c74ebf62fd1246a5b292d6e78f7070be6c5b90
Lucian <lucian6@qq.com>
2019-07-17 21:55:02 +0800

    fix: fix the travis ci error

--- test.sh
+++ test.sh
@@ -93,6 +93,7 @@ function check_git_diff() {
     # Set default args when first time check git diff
     yes | git difftool --extcmd icdiff > /dev/null
     git config --global icdiff.options '--cols=80'
+    export PATH=$PATH:"$(pwd)/git-icdiff"
   fi
   local tmp=/tmp/git-icdiff.output
   git icdiff $1 $2 &> $tmp
