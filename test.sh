5601d7096b54efc2e25eeb5e482b8e59989ad02e
Michael Klemm <michael@dontknow.de>
2022-02-21 19:47:56 +0100

    Set process exit code for shell in case of errors and file differences (#195)
    Mimic the behavior of grep. Return 1 to the invoking shell if differences between the icdiff'ed files have been detected. Return 2 for bad arguments.

--- test.sh
+++ test.sh
@@ -44,7 +44,10 @@ function fail() {
 }
 
 function check_gold() {
-  local gold=tests/$1
+  local error_code
+  local expect=$1
+  local gold=tests/$2
+  shift
   shift
 
   if [ $TEST_NAME != "all" -a $TEST_NAME != $gold ]; then
@@ -54,6 +57,7 @@ function check_gold() {
   echo "    check_gold $gold matches $@"
   local tmp=/tmp/icdiff.output
   $INVOCATION "$@" &> $tmp
+  error_code=$?
 
   if $REGOLD; then
     if [ -e $gold ] && diff $tmp $gold > /dev/null; then
@@ -77,6 +81,12 @@ function check_gold() {
     cat $tmp
     echo "Expected: ($gold)"
     cat $gold
+    fail
+  fi
+
+  if [[ $error_code != $expect ]]; then
+    echo "Got error code:      $error_code"
+    echo "Expected error code: $expect"
     fail
   fi
 }
@@ -105,47 +115,47 @@ function check_git_diff() {
   fi
 }
 
-check_gold gold-recursive.txt       --recursive tests/{a,b} --cols=80
-check_gold gold-exclude.txt         --exclude-lines '^#|  pad' tests/input-4-cr.txt tests/input-4-partial-cr.txt --cols=80
-check_gold gold-dir.txt             tests/{a,b} --cols=80
-check_gold gold-12.txt              tests/input-{1,2}.txt --cols=80
-check_gold gold-12-t.txt            tests/input-{1,2}.txt --cols=80 --truncate
-check_gold gold-3.txt               tests/input-{3,3}.txt
-check_gold gold-45.txt              tests/input-{4,5}.txt --cols=80
-check_gold gold-45-95.txt           tests/input-{4,5}.txt --cols=95
-check_gold gold-45-sas.txt          tests/input-{4,5}.txt --cols=80 --show-all-spaces
-check_gold gold-45-h.txt            tests/input-{4,5}.txt --cols=80 --highlight
-check_gold gold-45-nb.txt           tests/input-{4,5}.txt --cols=80 --no-bold
-check_gold gold-45-sas-h.txt        tests/input-{4,5}.txt --cols=80 --show-all-spaces --highlight
-check_gold gold-45-sas-h-nb.txt     tests/input-{4,5}.txt --cols=80 --show-all-spaces --highlight --no-bold
-check_gold gold-45-h-nb.txt         tests/input-{4,5}.txt --cols=80 --highlight --no-bold
-check_gold gold-45-ln.txt           tests/input-{4,5}.txt --cols=80 --line-numbers
-check_gold gold-45-ln-color.txt     tests/input-{4,5}.txt --cols=80 --line-numbers --color-map='line-numbers:cyan'
-check_gold gold-45-nh.txt           tests/input-{4,5}.txt --cols=80 --no-headers
-check_gold gold-45-h3.txt           tests/input-{4,5}.txt --cols=80 --head=3
-check_gold gold-45-l.txt            tests/input-{4,5}.txt --cols=80 -L left
-check_gold gold-45-lr.txt           tests/input-{4,5}.txt --cols=80 -L left -L right
-check_gold gold-45-lbrb.txt         tests/input-{4,5}.txt --cols=80 -L "L {basename}" -L "R {basename}"
-check_gold gold-45-pipe.txt         tests/input-4.txt <(cat tests/input-5.txt) --cols=80 --no-headers
-check_gold gold-4dn.txt             tests/input-4.txt /dev/null --cols=80 -L left -L right
-check_gold gold-dn5.txt             /dev/null tests/input-5.txt --cols=80 -L left -L right
-check_gold gold-67.txt              tests/input-{6,7}.txt --cols=80
-check_gold gold-67-wf.txt           tests/input-{6,7}.txt --cols=80 --whole-file
-check_gold gold-67-ln.txt           tests/input-{6,7}.txt --cols=80 --line-numbers
-check_gold gold-67-u3.txt           tests/input-{6,7}.txt --cols=80 -U 3
-check_gold gold-tabs-default.txt    tests/input-{8,9}.txt --cols=80
-check_gold gold-tabs-4.txt          tests/input-{8,9}.txt --cols=80 --tabsize=4
-check_gold gold-file-not-found.txt  tests/input-4.txt nonexistent_file
-check_gold gold-strip-cr-off.txt    tests/input-4.txt tests/input-4-cr.txt --cols=80
-check_gold gold-strip-cr-on.txt     tests/input-4.txt tests/input-4-cr.txt --cols=80 --strip-trailing-cr
-check_gold gold-no-cr-indent        tests/input-4-cr.txt tests/input-4-partial-cr.txt --cols=80
-check_gold gold-hide-cr-if-dos      tests/input-4-cr.txt tests/input-5-cr.txt --cols=80
-check_gold gold-12-subcolors.txt    tests/input-{1,2}.txt --cols=80 --color-map='change:magenta,description:cyan_bold'
-check_gold gold-subcolors-bad-color tests/input-{1,2}.txt --cols=80 --color-map='change:mageta,description:cyan_bold'
-check_gold gold-subcolors-bad-cat tests/input-{1,2}.txt --cols=80 --color-map='chnge:magenta,description:cyan_bold'
-check_gold gold-subcolors-bad-fmt tests/input-{1,2}.txt --cols=80 --color-map='change:magenta:gold,description:cyan_bold'
-check_gold gold-identical-on.txt tests/input-{1,1}.txt -s
-check_gold gold-bad-encoding.txt tests/input-{1,2}.txt --encoding=nonexistend_encoding
+check_gold 1 gold-recursive.txt       --recursive tests/{a,b} --cols=80
+check_gold 1 gold-exclude.txt         --exclude-lines '^#|  pad' tests/input-4-cr.txt tests/input-4-partial-cr.txt --cols=80
+check_gold 0 gold-dir.txt             tests/{a,b} --cols=80
+check_gold 1 gold-12.txt              tests/input-{1,2}.txt --cols=80
+check_gold 1 gold-12-t.txt            tests/input-{1,2}.txt --cols=80 --truncate
+check_gold 0 gold-3.txt               tests/input-{3,3}.txt
+check_gold 1 gold-45.txt              tests/input-{4,5}.txt --cols=80
+check_gold 1 gold-45-95.txt           tests/input-{4,5}.txt --cols=95
+check_gold 1 gold-45-sas.txt          tests/input-{4,5}.txt --cols=80 --show-all-spaces
+check_gold 1 gold-45-h.txt            tests/input-{4,5}.txt --cols=80 --highlight
+check_gold 1 gold-45-nb.txt           tests/input-{4,5}.txt --cols=80 --no-bold
+check_gold 1 gold-45-sas-h.txt        tests/input-{4,5}.txt --cols=80 --show-all-spaces --highlight
+check_gold 1 gold-45-sas-h-nb.txt     tests/input-{4,5}.txt --cols=80 --show-all-spaces --highlight --no-bold
+check_gold 1 gold-45-h-nb.txt         tests/input-{4,5}.txt --cols=80 --highlight --no-bold
+check_gold 1 gold-45-ln.txt           tests/input-{4,5}.txt --cols=80 --line-numbers
+check_gold 1 gold-45-ln-color.txt     tests/input-{4,5}.txt --cols=80 --line-numbers --color-map='line-numbers:cyan'
+check_gold 1 gold-45-nh.txt           tests/input-{4,5}.txt --cols=80 --no-headers
+check_gold 1 gold-45-h3.txt           tests/input-{4,5}.txt --cols=80 --head=3
+check_gold 2 gold-45-l.txt            tests/input-{4,5}.txt --cols=80 -L left
+check_gold 1 gold-45-lr.txt           tests/input-{4,5}.txt --cols=80 -L left -L right
+check_gold 1 gold-45-lbrb.txt         tests/input-{4,5}.txt --cols=80 -L "L {basename}" -L "R {basename}"
+check_gold 1 gold-45-pipe.txt         tests/input-4.txt <(cat tests/input-5.txt) --cols=80 --no-headers
+check_gold 1 gold-4dn.txt             tests/input-4.txt /dev/null --cols=80 -L left -L right
+check_gold 1 gold-dn5.txt             /dev/null tests/input-5.txt --cols=80 -L left -L right
+check_gold 1 gold-67.txt              tests/input-{6,7}.txt --cols=80
+check_gold 1 gold-67-wf.txt           tests/input-{6,7}.txt --cols=80 --whole-file
+check_gold 1 gold-67-ln.txt           tests/input-{6,7}.txt --cols=80 --line-numbers
+check_gold 1 gold-67-u3.txt           tests/input-{6,7}.txt --cols=80 -U 3
+check_gold 1 gold-tabs-default.txt    tests/input-{8,9}.txt --cols=80
+check_gold 1 gold-tabs-4.txt          tests/input-{8,9}.txt --cols=80 --tabsize=4
+check_gold 2 gold-file-not-found.txt  tests/input-4.txt nonexistent_file
+check_gold 1 gold-strip-cr-off.txt    tests/input-4.txt tests/input-4-cr.txt --cols=80
+check_gold 1 gold-strip-cr-on.txt     tests/input-4.txt tests/input-4-cr.txt --cols=80 --strip-trailing-cr
+check_gold 1 gold-no-cr-indent        tests/input-4-cr.txt tests/input-4-partial-cr.txt --cols=80
+check_gold 1 gold-hide-cr-if-dos      tests/input-4-cr.txt tests/input-5-cr.txt --cols=80
+check_gold 1 gold-12-subcolors.txt    tests/input-{1,2}.txt --cols=80 --color-map='change:magenta,description:cyan_bold'
+check_gold 2 gold-subcolors-bad-color tests/input-{1,2}.txt --cols=80 --color-map='change:mageta,description:cyan_bold'
+check_gold 2 gold-subcolors-bad-cat tests/input-{1,2}.txt --cols=80 --color-map='chnge:magenta,description:cyan_bold'
+check_gold 2 gold-subcolors-bad-fmt tests/input-{1,2}.txt --cols=80 --color-map='change:magenta:gold,description:cyan_bold'
+check_gold 0 gold-identical-on.txt tests/input-{1,1}.txt -s
+check_gold 2 gold-bad-encoding.txt tests/input-{1,2}.txt --encoding=nonexistend_encoding
 
 if git show 4e86205629 &> /dev/null; then
   # We're in the repo, so test git.
